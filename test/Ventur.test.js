f1eccb356141df3c856679294d8ba4ae0eb8af97
German Lugo <g.lugo.dev@gmail.com>
2023-04-22 10:59:29 +0200

    Start test units.

--- test/Ventur.test.js
+++ test/Ventur.test.js
@@ -11,12 +11,17 @@ require('chai')
     })
 
     describe('First round of test', async () => {
-      it("Should assert msg.sender is _admin and treasuryBox", async () => {
-        const owner = await TreasuryInstance.owner()
-        const TreasuryBox = await TreasuryInstance.treasuryBox()
-  
-        assert.equal(owner, TreasuryBox, "Owner is Treasury Box.")
+      it("Should assert if admin has right total supply.", async () => {
+        const balanceOfAdmin = await VenturInstance.balanceOf('0x5c22DE4456535D1948cB64d952896f080D140651')
+        assert.equal(balanceOfAdmin, 250000000000000000000000000, "Owner is Treasury Box.")
       })
+
+      // it("Should assert if transfer pay proprely fee.", async () => {
+      //   const balanceOfCollective0 = await VenturInstance.balanceOf('0xb3DF7E479424a15E6720C79Cb6BC62934Dc5917c')
+      //   await VenturInstance.transfer('0x148536B0C60DD62f3B68F788cfe75c3c31F07906', '1000000000000000000', { from: '0x5c22DE4456535D1948cB64d952896f080D140651'} )
+      //   const balanceOfCollective1 = await VenturInstance.balanceOf('0xb3DF7E479424a15E6720C79Cb6BC62934Dc5917c')
+      //   assert.equal(balanceOfCollective1, '1000000000000000', "Owner is Treasury Box.")
+      // })
     })
 
   })
